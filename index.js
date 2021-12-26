a453c2b54319c720b8803c6b4687e9fffdb86ed5
daulen <daulenz@gmail.com>
2021-12-25 22:12:36 -0600

    Done

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const input = document.getElementById('input');
 console.log(input.id)
+input.addEventListener('click',function(){
+  alert('I was clicked!')
+})
+// function clickAlert(){
+//   alert('I was cliked!');
+// }
+// input.addEventListener('click',clickAlert);
+
+const input2 = document.getElementById('input2');
+console.log(input2.id)
 
 function clickAlert(){
-  alert('I was cliked!');
+  alert("Happy New Year!");
 }
-input.addEventListener('click',clickAlert);
+input2.addEventListener('click',clickAlert);
