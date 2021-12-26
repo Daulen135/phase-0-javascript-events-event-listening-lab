43cc9897292742363522ba78de30b5ac6d839633
daulen <daulenz@gmail.com>
2021-12-26 08:30:17 -0600

    Done

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
+
 const input = document.getElementById('input');
-console.log(input.id)
+console.log(input.id);
+
 input.addEventListener('click',function(){
-  alert('I was clicked!')
-})
-// function clickAlert(){
-//   alert('I was cliked!');
-// }
-// input.addEventListener('click',clickAlert);
+  alert('I was clicked');
+});
 
 const input2 = document.getElementById('input2');
-console.log(input2.id)
+console.log(input2);
 
-function clickAlert(){
-  alert("Happy New Year!");
-}
-input2.addEventListener('click',clickAlert);
+function happyNY(){
+  alert('Happy New Year!')
+};
+
+input2.addEventListener('click', happyNY);
+
+
+
+
+
+
+
+// const input = document.getElementById('input');
+// console.log(input.id)
+// input.addEventListener('click',function(){
+//   alert('I was clicked!')
+// })
+// // function clickAlert(){
+// //   alert('I was cliked!');
+// // }
+// // input.addEventListener('click',clickAlert);
+
+// const input2 = document.getElementById('input2');
+// console.log(input2.id)
+
+// function clickAlert(){
+//   alert("Happy New Year!");
+// }
+// input2.addEventListener('click',clickAlert);
